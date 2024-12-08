a0c124c1a0e4eb9fb3c5dc164d8df529dd5f972d
anoop raju <anoopraju31@gmail.com>
2024-12-08 23:46:11 +0530

    User Module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      envFilePath: '.env',
       isGlobal: true,
     }),
-    MongooseModule.forRoot(process.env.MONGODB_URL),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
     ProductModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
